7de441e34b7e460d9144a45f757580712b44571d
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-12 15:46:45 +0000

    passed landing and takeoff tests, and traffic control test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,36 @@
 class Airport 
 
-  def initialize
+  attr_writer :capacity
+
+  def initialize(options = {})
+    self.capacity = options.fetch(:capacity, capacity)
   end
 
-  def land(plane)
-    plane.ground
+  def capacity
+    @capacity ||= 50
   end
+
+  def planes
+    @planes ||= []
+  end
+
+  def receive(plane)
+    raise "Plane cannot land, the airport is full" if full?
+    planes << plane
+    plane.land
+  end
+
+  def request_takeoff(plane)
+    plane.take_off
+  end 
+
+  def full?
+    plane_count == capacity
+  end
+
+  def plane_count
+    planes.count
+  end
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,34 +1,33 @@
 require 'airport'
 
 # A plane currently in the airport can be requested to take off.
-#
-# No more planes can be added to the airport, if it's full.
-# It is up to you how many planes can land in the airport and how that is impermented.
-#
-# If the airport is full then no planes can land
+
 describe Airport do
-  let(:airport) { Airport.new }
+  let(:airport) {Airport.new(:capacity => 50)}
   let(:plane) {double :plane}
   
   context 'taking off and landing' do
 
     it 'a plane can land' do
-      expect(plane).to receive(:ground)
-      airport.land(plane)
+      expect(plane).to receive(:land)
+      airport.receive(plane)
     end
 
-    xit 'a plane can take off' do
-
+    it 'a plane can take off' do
+      expect(plane).to receive(:take_off)
+      airport.request_takeoff(plane)
     end
 
   end
 
   context 'traffic control' do
 
-    xit 'a plane cannot land if the airport is full' do
-
+    it 'a plane cannot land if the airport is full' do
+          allow(plane).to receive(:land)
+          expect{51.times{airport.receive(plane)}}.to raise_error(RuntimeError, "Plane cannot land, the airport is full")
     end
 
+  
 # Include a weather condition using a module.
 # The weather must be random and only have two states "sunny" or "stormy".
 # Try and take off a plane, but if the weather is stormy, the plane can not take off and must remain in the airport.
